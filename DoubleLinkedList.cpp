41d5d4479583303d29ca9e81b83460f6ac64ea68
yudhistirageraldo <yudhistirageraldo@gmail.com>
2026-04-30 13:03:31 +0700

    selesai

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -252,4 +252,11 @@ int main()
         default:
             cout << "Invalid option\n";
         }
+        cout << "\nPress Enter to continue...";
+        cin.ignore();
+        cin.get();
+        cout << endl;
+        system("clear");
+    } while (choice != '6');
+}
 
